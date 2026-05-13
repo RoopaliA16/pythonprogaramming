ded5696d02603ad5b6166a560e433ba764b92344
ROOPALI ANDHALE <roopaliandhale@ROOPALIs-MacBook-Air.local>
2026-05-13 17:04:16 +0530

    done

---
+++ fundamentals/lecture1.py
@@ -0,0 +1,82 @@
+'''
+question: Ask user to enter the two numbers and do the addition of two numbers
+
+logic:
+
+1. I will ask user to enter two numbers
+2. Two numbers will be assigned to two variables
+3. One variables : ans = variable1 + variable2
+4. I will print the answer
+
+Execution :
+
+1. input () : will take user input as string and return it back to caller function
+2. assign two variables with input ():
+variablel1 =int(input()) : here we are converting the user input into integer number (int()) so 
+3. assign variable : ans : variable1 + variable2
+4. print the output 
+ 
+
+'''
+# %%
+# Ask user to enter two numbers
+variable1 = int(input("Enter the first number:"))
+variable2 = int(input("Enter the second number;"))
+
+ans = variable1 + variable2
+
+print(ans)
+ 
+
+print(type(ans))
+
+
+name = input ("enter the name : ")
+
+print (type(name))
+
+# %%
+# data types of python programming 
+
+'''
+1. integer
+2. float 
+3. string
+4. boolean
+5. list 
+6. tupel
+7. sets
+8. dictionary
+
+'''
+
+# %%
+
+
+pi = 3.421 
+
+name = 'india'
+
+India, pak, usa, = 100, 200.4545,'usaaa'
+ 
+a = True
+b = False
+
+print (a and b) 
+print (a or b)
+
+
+
+
+
+
+'''
+multi assignment veriable pythonprogramming
+
+boolean operter a an b same 
+
+'''
+
+
+
+
